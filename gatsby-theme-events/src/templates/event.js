1aba2e8e9d60c9d69a8f4743a5652f36c79c236c
Zain Fathoni <zain.fathoni@gmail.com>
2020-10-26 17:38:18 +0800

    Display and query data by id with context and static queries

--- gatsby-theme-events/src/templates/event.js
+++ gatsby-theme-events/src/templates/event.js
@@ -1,4 +1,18 @@
 import React from "react"
+import { graphql } from "gatsby"
+
+export const query = graphql`
+  query($eventID: String!) {
+    event(id: { eq: $eventID }) {
+      name
+      url
+      startDate(formatString: "MMMM DD YYYY")
+      endDate(formatString: "MMMM DD YYYY")
+      location
+      slug
+    }
+  }
+`
 
 const EventTemplate = () => <p>TODO: Build the event page template</p>
 
