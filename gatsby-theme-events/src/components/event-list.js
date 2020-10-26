04bdbc9bcdcf39c9eb17bb8a851b27d99cc7cbde
Zain Fathoni <zain.fathoni@gmail.com>
2020-10-26 17:37:24 +0800

    Update the event list component

--- gatsby-theme-events/src/components/event-list.js
+++ gatsby-theme-events/src/components/event-list.js
@@ -1,5 +1,26 @@
 import React from "react"
+import { Link } from "gatsby"
 
-const EventList = ({ events }) => <pre>{JSON.stringify(events, null, 2)}</pre>
+const EventList = ({ events }) => (
+  <>
+    <h2>Upcoming Events</h2>
+    <ul>
+      {events.map(event => (
+        <li key={event.id}>
+          <strong>
+            <Link to={event.slug}>{event.name}</Link>
+          </strong>
+          <br />
+          {new Date(event.startDate).toLocaleDateString("en-US", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          })}{" "}
+          in {event.location}
+        </li>
+      ))}
+    </ul>
+  </>
+)
 
 export default EventList
