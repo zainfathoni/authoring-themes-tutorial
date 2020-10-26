c2b1a9b4b44a7a4bcaa7ee491217d41c35ff3db0
Zain Fathoni <zain.fathoni@gmail.com>
2020-10-26 17:33:10 +0800

    Create the “events” and “event” template components

---
+++ gatsby-theme-events/src/templates/event.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const EventTemplate = () => <p>TODO: Build the event page template</p>
+
+export default EventTemplate
---
+++ gatsby-theme-events/src/templates/events.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const EventsTemplate = () => <p>TODO: Build the events page template</p>
+
+export default EventsTemplate
